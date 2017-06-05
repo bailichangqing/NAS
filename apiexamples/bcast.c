a34b1e3beecdbca961fa238396841b6df214d973
Conghao Liu <cliu115@hawk.iit.edu>
2017-06-05 00:55:51 -0500

    add example for running time comparison of MPI_Bcast in C and Julia

---
+++ apiexamples/bcast.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<mpi.h>
+#include<time.h>
+
+int main()
+{
+	clock_t start,end;
+
+	MPI_Init(NULL,NULL);
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	
+	int buffer[3];
+	
+	if(rank == 0)
+	{
+		buffer[0] = 1;
+		buffer[1] = 3;
+		buffer[2] = 5;
+	}
+	else if(rank == 1)
+	{
+		buffer[0] = 2;
+		buffer[1] = 4;
+		buffer[2] = 6;
+	}
+	start = clock();
+	MPI_Bcast(buffer,3,MPI_INT,1,MPI_COMM_WORLD);
+	end = clock();
+	MPI_Finalize();
+	if(rank == 0)
+		printf("cputime elapsed:	%f\n",((double)(end - start))/CLOCKS_PER_SEC);
+	return 0;
+}
